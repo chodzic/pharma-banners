75db09ff6b118353ce66ca4e4cb8688e555a8b44
qamhoxha <qamcode@gmail.com>
2020-04-12 13:18:47 -0400

    added ISI element properties when a user shows the entire ISI in the console

--- src/console/actions/actions.js
+++ src/console/actions/actions.js
@@ -24,12 +24,112 @@ for (var i = 0; i < onoff.length; i++) {
 
 let isi_placeholder = document.createElement('div');
 isi_placeholder.classList.add("isi-fpo");
-isi_placeholder.innerHTML = "<span>The ISI has been repositioned below the banner's canvas for routing purposes.";
+isi_placeholder.innerHTML = "<span>The ISI has been repositioned below the banner's canvas for routing purposes.</span>";
+
+let isi_properties = document.createElement('div');
+isi_properties.classList.add("isi-properties");
+
+var el_isi = document.querySelector(".isi-wrapper");
+var el_isi_h2 = document.querySelector(".isi-wrapper h2");
+var el_isi_h3 = document.querySelector(".isi-wrapper h3");
+var el_isi_p = document.querySelector(".isi-wrapper p");
+var el_isi_ul = document.querySelector(".isi-wrapper ul");
+var el_isi_li = document.querySelector(".isi-wrapper ul li");
+var el_isi_a = document.querySelector(".isi-wrapper a");
+
+function getElComputedStyle(el,prop) {
+	return window.getComputedStyle(el, null).getPropertyValue(prop);
+}
+
+
+function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+
+var prop_array = [
+	["General", 
+		[
+			[ "font-family", getElComputedStyle(el_isi,"font-family") ],
+			[ "font-size", getElComputedStyle(el_isi,"font-size") ],
+			[ "font-weight", getElComputedStyle(el_isi,"font-weight") ],
+			[ "color", rgb2hex(getElComputedStyle(el_isi,"color")) ]
+		]
+	],
+	["Headline", 
+		[
+			[ "font-family", getElComputedStyle(el_isi_h2,"font-family") ],
+			[ "font-size", getElComputedStyle(el_isi_h2,"font-size") ],
+			[ "font-weight", getElComputedStyle(el_isi_h2,"font-weight") ],
+			[ "line-height", getElComputedStyle(el_isi_h2,"line-height") ],
+			[ "color", rgb2hex(getElComputedStyle(el_isi_h2,"color")) ]
+		] 
+	],
+	["Subhead", 
+		[
+			[ "font-family", getElComputedStyle(el_isi_h3,"font-family") ],
+			[ "font-size", getElComputedStyle(el_isi_h3,"font-size") ],
+			[ "font-weight", getElComputedStyle(el_isi_h3,"font-weight") ],
+			[ "line-height", getElComputedStyle(el_isi_h2,"line-height") ],
+			[ "color", rgb2hex(getElComputedStyle(el_isi_h2,"color")) ]
+		] 
+	],
+	["Paragraph", 
+		[
+			[ "font-family", getElComputedStyle(el_isi_p,"font-family") ],
+			[ "font-size", getElComputedStyle(el_isi_p,"font-size") ],
+			[ "font-weight", getElComputedStyle(el_isi_p,"font-weight") ],
+			[ "line-height", getElComputedStyle(el_isi_p,"line-height") ],
+			[ "color", rgb2hex(getElComputedStyle(el_isi_p,"color")) ]
+		] 
+	],
+	["List Item", 
+		[
+			[ "font-family", getElComputedStyle(el_isi_li,"font-family") ],
+			[ "font-size", getElComputedStyle(el_isi_li,"font-size") ],
+			[ "font-weight", getElComputedStyle(el_isi_li,"font-weight") ],
+			[ "line-height", getElComputedStyle(el_isi_li,"line-height") ],
+			[ "color", rgb2hex(getElComputedStyle(el_isi_li,"color")) ],
+			[ "padding-left", getElComputedStyle(el_isi_ul,"padding-left") ]
+		] 
+	],
+	["Link", 
+		[
+			[ "font-family", getElComputedStyle(el_isi_a,"font-family") ],
+			[ "font-size", getElComputedStyle(el_isi_a,"font-size") ],
+			[ "font-weight", getElComputedStyle(el_isi_a,"font-weight") ],
+			[ "color", rgb2hex(getElComputedStyle(el_isi_a,"color")) ]
+		] 
+	]
+];
+
+
+
+
+var prop_html = "";
+for (var i=0; i<prop_array.length; i++) {
+	prop_html += "<div class='element'><div class='el-type'>"+prop_array[i][0]+"</div>";
+
+	for (var p=0; p<(prop_array[i][1].length);p++) {
+
+		prop_html += "<div class='prop-wrap'><div class='selector'>"+prop_array[i][1][p][0]+":</div><div class='value'>"+prop_array[i][1][p][1]+"</div></div>"
+	}
+	prop_html += "</div>";
+}
+
+isi_properties.innerHTML = prop_html;
+
+
 
 document.querySelector('[data-toggleclass=isi-show]').addEventListener('click', function(e){
 	let isi = document.querySelector("#isi");
 	isi_placeholder.style.height = isi.offsetHeight + "px";
 	isi_placeholder.style.width = isi.offsetWidth + "px";
 	isi.insertBefore(isi_placeholder, isi.firstElementChild);
+	isi.insertBefore(isi_properties, isi.firstElementChild);
 });
 
